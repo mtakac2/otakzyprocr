6e09c0f5531ae2bb3550e80597612b97f3afa110
Marek Takac <takacmarek1221@gmail.com>
2012-07-29 00:26:43 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Otazkyprocr::Application.config.secret_token = 'f62c91f220651389d51acf88effcfa6f46e48903697eccf0fabae30c5c7da0baa709c91c38369fb63f491097abb81bf9d290aef66de1632068f3026e4f0a305d'
