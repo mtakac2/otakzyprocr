576db857bda41ba1abb3d788f4527a806f3d424c
Marek Takac <takacmarek1221@gmail.com>
2012-09-11 20:27:24 +0200

    Create update action for politicians and parties controller

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,3 +1,5 @@
+# encoding: UTF-8
+
 class PartiesController < ApplicationController
   def show
     @party = Refinery::Parties::Party.find(params[:id])
--- app/controllers/politicians_controller.rb
+++ app/controllers/politicians_controller.rb
@@ -1,3 +1,5 @@
+# encoding: UTF-8
+
 class PoliticiansController < ApplicationController
   def show
     @politician = Refinery::Politicians::Politician.find(params[:id])
