d9b510a27495af118d1b0ab39d981831ffc5736c
Marek Takac <takacmarek1221@gmail.com>
2012-09-17 17:23:04 +0200

    Paypal notifications 2

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -3,12 +3,13 @@
 class PaymentsController < ApplicationController
   def paypal
     @citizens_question = session[:citizens_question]
-
+    session[:citizens_question] = nil
+    
     if @citizens_question.save
       @status = 'Vaše platba proběhla úspěšne. Doklad o platbě byl zaslán na Váš email. Pro více detailů se múžete přihlásit do svého
         paypal účtu: www.sandbox.paypal.com/. Děkujeme.'
     else
       @status = 'Byli zaznamenány problémy s platbou. Prosím kontaktujte administrátora.'
-    end
+    end    
   end
 end
