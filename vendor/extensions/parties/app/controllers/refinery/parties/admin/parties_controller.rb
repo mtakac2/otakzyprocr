f89cd7a0682edfe5b9e909f7956b400ec1eebb2b
Marek Takac <takacmarek1221@gmail.com>
2012-09-24 17:05:18 +0200

    Add functionality for keeper to have multiple parties

--- vendor/extensions/parties/app/controllers/refinery/parties/admin/parties_controller.rb
+++ vendor/extensions/parties/app/controllers/refinery/parties/admin/parties_controller.rb
@@ -6,6 +6,24 @@ module Refinery
         crudify :'refinery/parties/party',
                 :title_attribute => 'name', :xhr_paging => true
 
+        def new
+          @party = Party.new
+          @keepers = Refinery::Keepers::Keeper.order('email ASC')
+          @elections = Refinery::Elections::Election.order('held DESC')
+        end
+
+        def create
+          @party = Party.new(params[:party])
+          @election_subject_election = ElectionSubjectElection.new(params[:election_subject_election])          
+
+          if @party.save
+            @election_subject_election.subject_id = @party.id
+            @election_subject_election.save
+            redirect_to refinery.parties_admin_parties_path
+          else
+            render 'new'
+          end
+        end
       end
     end
   end
