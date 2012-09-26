3aae14c5e7cb1d40e81da843b66439358d735d93
Marek Takac <takacmarek1221@gmail.com>
2012-09-26 17:22:19 +0200

    Paypal 4

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,10 +2,12 @@
 
 class PaymentsController < ApplicationController
   def paypal
+    """
     @citizens_question = CitizensQuestion.new(citizen_id: session[:citizens_question][:citizen_id], question_id: session[:citizens_question][:question_id],
       hours: session[:citizens_question][:hours])
     session[:citizens_question] = nil
-    
+    """
+
     """
     if @citizens_question.save
       @status = 'Vaše platba proběhla úspěšne. Doklad o platbě byl zaslán na Váš email. Pro více detailů se múžete přihlásit do svého
