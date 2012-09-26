75077a5c54a72c2268c9679bee71cc6950667fa9
Marek Takac <takacmarek1221@gmail.com>
2012-09-26 17:41:35 +0200

    Paypal 6

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,19 +2,18 @@
 
 class PaymentsController < ApplicationController
   def paypal
-    """
-    @citizens_question = CitizensQuestion.new(citizen_id: session[:citizens_question][:citizen_id], question_id: session[:citizens_question][:question_id],
-      hours: session[:citizens_question][:hours])
-    session[:citizens_question] = nil
-    """
-
-    """
+    
+    @citizens_question = CitizensQuestion.new(citizen_id: session[:c_q_citizen_id], question_id: session[:c_q_question_id],
+      hours: session[:c_q_hours])
+    session[:c_q_citizen_id] = nil
+    session[:c_q_question_id] = nil
+    session[:c_q_hours] = nil
+        
     if @citizens_question.save
       @status = 'Vaše platba proběhla úspěšne. Doklad o platbě byl zaslán na Váš email. Pro více detailů se múžete přihlásit do svého
         paypal účtu: www.paypal.com/us. Děkujeme.'
     else
       @status = 'Byli zaznamenány problémy s platbou. Prosím kontaktujte administrátora.'
     end
-    """    
   end
 end
