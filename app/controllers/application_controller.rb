72a1371ea37a03a246e228bbc889b0df080ac5f5
otazky <milus.kotisova@volny.cz>
2012-11-01 13:35:50 +0100

    Update app/controllers/application_controller.rb
    čeština - vyberte

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 
     def block_citizen_without_question
       if session[:user_type] == 'Refinery::Citizens::Citizen' && current_user.questions.empty?
-        redirect_to main_app.questions_path, :notice => 'Veberte otazku!!!'
+        redirect_to main_app.questions_path, :notice => 'Vyberte otazku!!!'
       end
     end
 
